a0fb25baa52e5499046b06a6b0c9fe7d26dc8c7e
pavan <pavan@beehyv.com>
2020-06-29 10:14:40 +0530

    update

--- demo/src/main/java/com/jenkins/demo/HelloWorld.java
+++ demo/src/main/java/com/jenkins/demo/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld {
 
     @RequestMapping("/Hello")
     public String hello() {
-        return "Hello, Good morning! Ready for demo";
+        return "Hello, Good morning! Final Test for demo";
     }
 }
